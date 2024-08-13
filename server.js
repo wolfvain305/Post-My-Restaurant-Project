212478928978f6553629fc9c455841e2edc258dd
DrueWolf <dwolf0305@gmail.com>
2024-08-13 19:42:15 -0400

    got most of the base files and folders in, some middle ware that i need, more to come later

---
+++ server.js
@@ -0,0 +1,31 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const methodOverride = require ('method-override')
+const mongoose = require('mongoose')
+const MONGO_URI = process.env.MONGO_URI
+const authorRouter = require('./routes/authors')
+
+mongoose.connect(MONGO_URI)
+
+mongoose.connection.once('open', () => {
+    console.log('You did it i am connected to Mongo')
+})
+
+mongoose.connection.on('error', () => {
+    console.log('DOnt get mad but there is an error')
+})
+
+app.use(express.urlencoded({ extended: true}))
+app.use((req, res, next) => {
+    res.locals.data = {}
+    next()
+})
+app.use(methodOverride('_method'))
+app.use('/assets', express.static('public'))
+
+app.use('/authors', authorRouter)
+
+app.listen(3000, () => {
+    console.log('Running the app on 3000')
+})
